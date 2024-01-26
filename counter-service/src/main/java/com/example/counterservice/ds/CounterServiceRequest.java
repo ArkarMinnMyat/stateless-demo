652d14597ab4edffef5ee5f48b507d6d6756b63e
HackerMgPyone <arkarminmyat121@gmail.com>
2024-01-26 09:30:52 +0630

    Initial commit

---
+++ counter-service/src/main/java/com/example/counterservice/ds/CounterServiceRequest.java
@@ -0,0 +1,14 @@
+package com.example.counterservice.ds;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class CounterServiceRequest {
+
+    private int number;
+
+    public CounterServiceRequest(int number) {
+        this.number = number;
+    }
+}
